295c18237ffc2dba086249bda98be5a073e9967f
angeliquee10 <angeliquee10@nycstudents.net>
2025-11-19 08:42:16 -0500

    Add files via upload
    11/19 update

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,76 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //this is testing my score method
+//        String message = "oh no";
+//        System.out.println(Methods.score(message));
+
+        // below are going to be all the final messages/fortunes
+        String fortune1 = "";
+        String fortune2 = "";
+        String fortune3 = "";
+        String fortune4 = "";
+        String fortune5 = "";
+        String fortune6 = "";
+        String fortune7 = "";
+        String fortune8 = "";
+
+
+
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Please enter your name: ");
         String name = scan.nextLine();
-
-
+        System.out.println("Welcome " + name + "! Let us tell your fortune...");
+        System.out.println("First pick a number  1 or 2");
+        int round1 = scan.nextInt();
+        // 1 and 2 will have 2 separate question choices, i will then according to the number chosen, will use random to pick one of the 2 questions
+        if (round1 == 1)
+        {
+            System.out.print("Next pick red or orange: ");
+            String round2 = scan.nextLine().toLowerCase();
+            if (round2.equals("red"))
+            {
+                System.out.print("Next pick left or right: ");
+                String round3 = scan.nextLine().toLowerCase();
+                if (round3.equals("left"))
+                {}
+                else
+                {}
+            }
+            else
+            {
+                System.out.print("Next pick left or right: ");
+                String round3 = scan.nextLine().toLowerCase();
+                if (round3.equals("left"))
+                {}
+                else
+                {}
+            }
+        }
+        else
+        {
+            System.out.print("Next pick green or blue: ");
+            String round2 = scan.nextLine().toLowerCase();
+            if (round2.equals("green"))
+            {
+                System.out.print("Next pick left or right: ");
+                String round3 = scan.nextLine().toLowerCase();
+                if (round3.equals("left"))
+                {}
+                else
+                {}
+            }
+            else
+            {
+                System.out.print("Next pick left or right: ");
+                String round3 = scan.nextLine().toLowerCase();
+                if (round3.equals("left"))
+                {}
+                else
+                {}
+            }
+        }
 
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,10 +1,10 @@
 public class Methods {
-    public int score(String finalMessage)
+    public static int score(String finalMessage)
     {
         finalMessage = finalMessage.toLowerCase();
         int score = 0;
         String current = "";
-        for (int i = 0; i < finalMessage.length() - 1; i++)
+        for (int i = 0; i < finalMessage.length(); i++)
         {
             current = finalMessage.substring(i, i + 1);
             if (current.equals("a") | current.equals("e") | current.equals("i") | current.equals("o") | current.equals("u"))
@@ -19,6 +19,7 @@ public class Methods {
             {
                 score ++;
             }
+//            System.out.println(score);
         }
         return score;
     }
